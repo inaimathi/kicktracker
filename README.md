df81c9ad8f78080b6b886684548e4ad839533493
inaimathi <inaimathi@self>
2015-04-23 21:46:14 -0400

    -Now escaping creator-name as well as the other text fields -Added "card game" search to the /board-games handler (was missing Apocrypha) -Added a bunch of README notes

--- README.md
+++ README.md
@@ -4,7 +4,33 @@ I'm sick of [missing](https://www.kickstarter.com/projects/maydaygames/viceroy-f
 
 So I made this.
 
-I may deploy it somewhere at some point.
+It's currently deployed and running at [kicktracker.inaimathi.ca](http://kicktracker.inaimathi.ca).
+
+## Usage
+
+#### Client
+
+It's an RSS feed server, so just go it and pick a feed. It's currently running at [kicktracker.inaimathi.ca](http://kicktracker.inaimathi.ca). Among the feeds you can subscribe to are:
+
+- `/recently-launched`
+- `/staff-picks`
+- `/board-games`
+- `/by-category/id/<category-id>`
+	- [`/by-category/id/15`](http://kicktracker.inaimathi.ca/by-category/id/15) *(the feed for Photography)*
+	- [`/by-category/id/3`](http://kicktracker.inaimathi.ca/by-category/id/3) *(the feed for Comics)*
+- `/custom/<search-term>`
+	- [`/custom/ryan laukat`](http://kicktracker.inaimathi.ca/custom/ryan laukat) *(the feed for Red Raven games)*
+	- [`/custom/robot`](http://kicktracker.inaimathi.ca/custom/robot) *(the feed for robots and robot-related stuff)*
+
+All feeds are sorted in descending order of launch-date.
+
+#### Server
+
+- Clone this repo and `cd` into it
+- Run `lein uberjar`
+- Run `java -jar target/kicktracker*standalone.jar`
+
+You can specify the listening port in that last step by passing it as an argument. The default port is `8000`. For instance, `java -jar target/kicktracker*standalone.jar 8484` listens on port `8484` instead of the default.
 
 ## TODO
 
--- src/kicktracker/core.clj
+++ src/kicktracker/core.clj
@@ -28,7 +28,7 @@
                       [:updated (clj-time.coerce/from-long (p :launched-at))]
                       [:summary (html-escape (p :blurb))]
                       [:content {:type "html"} [:img {:src (p :pic)}]]
-                      [:author [:name (p :creator)]]])
+                      [:author [:name (html-escape (p :creator))]]])
                    ps)])))})
 
 (defn home []
@@ -45,7 +45,7 @@
     [:ul 
      [:li [:a {:href "/recently-launched"} [:b [:code "/recently-launched"]]] " - covers all recently launched projects. This one tends to move pretty fast."]
      [:li [:a {:href "/staff-picks"} [:b [:code "/staff-picks"]]] " - covers the projects hand-picked by the Kickstarter staff."]
-     [:li [:a {:href "/board-games"} [:b [:code "/board-games"]]] " - covers most of the things I'm interested in. It's a combination of searches for \"tabletop game\", \"board game\" and category #34"]
+     [:li [:a {:href "/board-games"} [:b [:code "/board-games"]]] " - covers most of the things I'm interested in. It's a combination of searches for \"tabletop game\", \"board game\", \"card game\" and category #34"]
      [:li [:b [:code "/by-category/id/[category-id-int]"]] " - lets you pick a category to subscribe to. For instance, " [:a {:href "/by-category/id/3"} [:code "/by-category/id/3"]] " will give you the Comics feed."]
      [:li [:b [:code "/custom/[search-term]"]] " - lets you get the feed of an arbitrary search term. For instance, " [:a {:href "/custom/ryan laukat"} [:code "/custom/ryan laukat"]] " shows you the feed of all kickstarted "  [:a {:href "http://www.redravengames.com/"} "Red Raven games"] ". As of this writing, there aren't any new ones, but that's what these feeds are for."]]]))
 
@@ -63,6 +63,7 @@
                       (clojure.set/union 
                        (q/search "tabletop+game")
                        (q/search "board+game")
+                       (q/search "card+game")
                        (q/by-category (q/category :board-games))))
       (cond 
         (.startsWith uri "/by-category/id/") (let [[_ num-str] (re-find #"/by-category/id/(\d+)" uri)
