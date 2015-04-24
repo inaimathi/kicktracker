28cfa216d4c81149e95a718489a3dfa975733779
inaimathi <inaimathi@self>
2015-04-23 21:05:50 -0400

    -Added intro home page

--- src/kicktracker/core.clj
+++ src/kicktracker/core.clj
@@ -12,51 +12,69 @@
   (clojure.string/escape str {\< "&lt;", \> "&gt;", \& "&amp;"}))
 
 (defn projects->feed [title subtitle projects]
-  (let [ps (sort-by :launched-at > projects)]
-    (str "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-         (hic/html
-          [:feed {:xmlns "http://www.w3.org/2005/Atom"}
-           [:title title] [:subtitle subtitle]
-           [:link {:href "http://kicktracker.inaimathi.ca/feed/atom" :rel "self"}]
-           [:link {:href "http://kicktracker.inaimathi.ca"}]
-           (map
-            (fn [p]
-              [:entry 
-               [:title (html-escape (p :name))] [:link {:href (p :url)}] [:id (str "project-" (p :id))]
-               [:updated (clj-time.coerce/from-long (p :launched-at))]
-               [:summary (html-escape (p :blurb))]
-               [:content {:type "html"} [:img {:src (p :pic)}]]
-               [:author [:name (p :creator)]]])
-            ps)]))))
+  {:status 200
+   :headers {"Content-Type" "application/atom+xml"}
+   :body (let [ps (sort-by :launched-at > projects)]
+           (str "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+                (hic/html
+                 [:feed {:xmlns "http://www.w3.org/2005/Atom"}
+                  [:title title] [:subtitle subtitle]
+                  [:link {:href "http://kicktracker.inaimathi.ca/feed/atom" :rel "self"}]
+                  [:link {:href "http://kicktracker.inaimathi.ca"}]
+                  (map
+                   (fn [p]
+                     [:entry 
+                      [:title (html-escape (p :name))] [:link {:href (p :url)}] [:id (str "project-" (p :id))]
+                      [:updated (clj-time.coerce/from-long (p :launched-at))]
+                      [:summary (html-escape (p :blurb))]
+                      [:content {:type "html"} [:img {:src (p :pic)}]]
+                      [:author [:name (p :creator)]]])
+                   ps)])))})
+
+(defn home []
+  (pg/html5
+   {:lang "en"}
+   [:head
+    [:meta {:content "width=device-width, initial-scale=1" :name "viewport"}]
+    [:title "Kicktracker: the Kickstarter RSS Feed"]]
+   [:body
+    [:h1 "Kicktracker"]
+    [:h3 [:i "The kickstarter RSS feed"]]
+    [:p "I got sick of missing interesting game projects on Kickstarter, so I made this."]
+    [:p "There are a number of ATOM feeds you can subscribe to here, all sorted by start-date starting from the most recent:"]
+    [:ul 
+     [:li [:a {:href "/recently-launched"} [:b [:code "/recently-launched"]]] " - covers all recently launched projects. This one tends to move pretty fast."]
+     [:li [:a {:href "/staff-picks"} [:b [:code "/staff-picks"]]] " - covers the projects hand-picked by the Kickstarter staff."]
+     [:li [:a {:href "/board-games"} [:b [:code "/board-games"]]] " - covers most of the things I'm interested in. It's a combination of searches for \"tabletop game\", \"board game\" and category #34"]
+     [:li [:b [:code "/by-category/id/[category-id-int]"]] " - lets you pick a category to subscribe to. For instance, " [:a {:href "/by-category/id/3"} [:code "/by-category/id/3"]] " will give you the Comics feed."]
+     [:li [:b [:code "/custom/[search-term]"]] " - lets you get the feed of an arbitrary search term. For instance, " [:a {:href "/custom/ryan laukat"} [:code "/custom/ryan laukat"]] " shows you the feed of all kickstarted "  [:a {:href "http://www.redravengames.com/"} "Red Raven games"] ". As of this writing, there aren't any new ones, but that's what these feeds are for."]]]))
 
 ;;;;;;;;;; Handlers
 (defn handler [req]
-  (let [uri (req :uri)
-        body (case uri
-               ("/" "/recently-launched") (projects->feed "Recent" "All recently launched projects" (q/recently-launched))
-               "/staff-picks" (projects->feed "Picks" "Recent KS staff picks" (q/staff-picks))
-               "/board-games" (projects->feed 
-                               "Boardgames" "Recently launched tabletop game projects" 
-                               (clojure.set/union 
-                                (q/search "tabletop+game")
-                                (q/search "board+game")
-                                (q/by-category (q/category :board-games))))
-               (cond 
-                 (.startsWith uri "/by-category/id/") (let [[_ num-str] (re-find #"/by-category/id/(\d+)" uri)
-                                                            num (Integer. num-str)]
-                                                        (projects->feed "Category Feed" (str "category feed for ID:" num-str) (q/by-category num)))
-                 (.startsWith uri "/custom/") (let [[match term] (re-find #"/custom/(.+)" uri)]
-                                                (projects->feed 
-                                                 "Custom Feed" (str "feed for the search term \"" (URLDecoder/decode term) "\"")
-                                                 (q/raw-search term)))
-                 :else nil))]
-    (if body
-      {:status 200
-       :headers {"Content-Type" "application/atom+xml"}
-       :body body}
-      {:status 404
-       :headers {"Content-Type" "text/plain"}
-       :body "Nope, don't have that..."})))
+  (let [uri (req :uri)]
+    (case uri
+      "/" {:status 200
+           :headers {"Content-Type" "text/html"}
+           :body (home)} 
+      "/recently-launched" (projects->feed "Recent" "All recently launched projects" (q/recently-launched))
+      "/staff-picks" (projects->feed "Picks" "Recent KS staff picks" (q/staff-picks))
+      "/board-games" (projects->feed 
+                      "Boardgames" "Recently launched tabletop game projects" 
+                      (clojure.set/union 
+                       (q/search "tabletop+game")
+                       (q/search "board+game")
+                       (q/by-category (q/category :board-games))))
+      (cond 
+        (.startsWith uri "/by-category/id/") (let [[_ num-str] (re-find #"/by-category/id/(\d+)" uri)
+                                                   num (Integer. num-str)]
+                                               (projects->feed "Category Feed" (str "category feed for ID:" num-str) (q/by-category num)))
+        (.startsWith uri "/custom/") (let [[match term] (re-find #"/custom/(.+)" uri)]
+                                       (projects->feed 
+                                        "Custom Feed" (str "feed for the search term \"" (URLDecoder/decode term) "\"")
+                                        (q/raw-search term)))
+        :else {:status 404
+               :headers {"Content-Type" "text/plain"}
+               :body "Nope, don't have that..."}))))
 
 ;;;;;;;;;; Main entry point
 (defn -main 
